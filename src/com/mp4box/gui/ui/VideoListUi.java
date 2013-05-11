ebf7e84a2c20b88b9ea0a4ea95430e4cc242bf83
Rune Andre Liland <runeandre@gmail.com>
2013-05-11 17:25:09 +0200

    Version 1.6

--- src/com/mp4box/gui/ui/VideoListUi.java
+++ src/com/mp4box/gui/ui/VideoListUi.java
@@ -252,7 +252,7 @@ public class VideoListUi extends JFrame implements DropTargetListener {
 	}
 	
 	private void init(){
-		this.setTitle("Java MP4Box Gui v1.6-SNAPSHOT");
+		this.setTitle("Java MP4Box Gui v1.6");
 		this.setSize(640,480);
 		this.setLocationRelativeTo(null); //Centers the window on the screen
 		this.pack();
