1fac8133e6df7764099f77e4ce31656cb7c5e8ad
Rune Andre Liland <runeandre@gmail.com>
2013-05-15 19:25:30 +0200

    Removed test code!

--- src/com/mp4box/gui/Main.java
+++ src/com/mp4box/gui/Main.java
@@ -1,7 +1,5 @@
 package com.mp4box.gui;
 
-import java.io.IOException;
-
 import com.mp4box.gui.ui.VideoListUi;
 
 public class Main {
@@ -11,37 +9,6 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		new VideoListUi();
-		
-//		boolean continueLoop = true;
-//		int i=1;
-//		while(continueLoop){
-//			String cmd = "cmd echo ";
-//			try {
-//				for(int j=0;j<i;j++){
-//					cmd += "a";
-//				}
-//				
-//				Runtime rt = Runtime.getRuntime();
-//				Process proc = rt.exec(cmd);
-//				
-//				System.out.println("String lengde: " + cmd.length());
-//				i++;
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//				continueLoop = false;
-//				System.out.println("String lengde: " + cmd.length());
-//			}
-//		}
-		
-//		int ådele = 5;
-//		int lengde = 235;
-//		int delt = lengde/ådele;
-//		int gjenværende = lengde-(delt*(ådele-1));
-//		int tilsammen = delt*(ådele-1) + gjenværende;
-//		
-//		System.out.println("Delt: " + delt);
-//		System.out.println("Gjenværende: " + gjenværende);
-//		System.out.println("Tilsammen: " + tilsammen);
 	}
 
 }
