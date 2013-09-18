c78513a3ac11d279b336cd96f9403add1ff1e337
Rune Andre Liland <devnull@localhost>
2013-09-18 20:57:28 +0200

    Version 1.8, The Handbrake edition!

--- src/com/mp4box/gui/ui/VideoListUi.java
+++ src/com/mp4box/gui/ui/VideoListUi.java
@@ -331,7 +331,7 @@ public class VideoListUi extends JFrame implements DropTargetListener {
 			e.printStackTrace();
 		}
 		
-		this.setTitle("Java MP4Box Gui v1.8-SNAPSHOT");
+		this.setTitle("Java MP4Box Gui v1.8");
 		this.setSize(640,480);
 		this.setLocationRelativeTo(null); //Centers the window on the screen
 		this.pack();
